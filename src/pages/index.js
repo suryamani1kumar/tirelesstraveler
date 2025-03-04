fc0317919b8da5ec662b1af94e4548da549111f3
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-04 15:46:41 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,15 @@ export default function Home() {
     <div>
       {/* https://fe7mnau930.onrocket.site/travelmatic/?s=italy */}
       Tireless Traveler
+    <iframe
+      id="mapFrame"
+      src="https://www.google.com/maps?q=Eiffel+Tower,Paris&output=embed"
+      style="border: 0; width: 99vw; height: 50vh; margin: 0 auto"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    >
+    </iframe>
       <Blog />
     </div>
   );
