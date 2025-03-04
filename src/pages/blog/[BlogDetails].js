9ee3fde0c54c8a8c0346d4cf57a72b46341e1a7b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-04 18:53:36 +0530

    Update [BlogDetails].js

--- src/pages/blog/[BlogDetails].js
+++ src/pages/blog/[BlogDetails].js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const BlogDetails = () => {
-  return <div>BlogDetails</div>;
+  return  <div className="related-posts">
+      <h3>You May Also Like</h3>
+      <div className="posts">
+        <div className="post">
+          <img
+            src="https://dummyimage.com/390x200/cfc0cf/ffffff"
+            alt="Weekend in Warsaw"
+          />
+          <a href="#">Weekend in Warsaw </a>
+
+          <span><button className="button">Continue Reading</button></span>
+        </div>
+        <div className="post">
+          <img
+            src="https://dummyimage.com/390x200/cfc0cf/ffffff"
+            alt="Two Days in Budapest"
+          />
+          <a href="#">Two Days in Budapest</a>
+
+          <span><button className="button">Continue Reading</button></span>
+        </div>
+      </div>
+    </div>;
 };
 
 export default BlogDetails;
