af8cb6b2364f1b76ae820a426e27c2d9a55eefef
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-11 07:22:43 +0530

    change in blog page

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,102 +1,96 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
+import styles from '../../styles/blog.module.css';
+import Link from 'next/link';
 
 const Blog = () => {
   return (
     <div className="container mx-auto px-4">
-      <div className="author-card">
-        <h4 className="author-title">AUTHOR</h4>
-        <div className="author-image">
-          <img src="https://www.dummyimage.com/300" alt="Author Image" />
+      <div className="flex justifty-sapce">
+        <div>
+          {Array(5)
+            .fill(0)
+            .map((item, i) => (
+              <div className="blog-card" key={i}>
+                <div className="blog-image">
+                  <Image
+                    src="/images/piatra-craiului-national-park-romania.jpg"
+                    alt="piatra-craiului-national-park-romania"
+                    width={0}
+                    height={0}
+                  />
+                </div>
+                <div className="blog-content">
+                  <h2 className="blog-title">Trip to Italy: Rome</h2>
+                  <p className="blog-meta">
+                    <span className="icon">🏷️</span> POSTED IN EUROPE
+                    <span className="icon">👤</span> KATHY
+                  </p>
+                  <p className="blog-description">
+                    This post is part of a series called Travel to Italy
+                    Palatine Hill. Aliquam sit amet euismod purus, sed commodo
+                    ipsum. Morbi sodales consectetur ex vitae molestie. Morbi
+                    ultrices rhoncus...
+                  </p>
+                  <Link href="/blog/sdasdsdasd" className="read-more">
+                    CONTINUE READING
+                  </Link>
+                </div>
+              </div>
+            ))}
         </div>
-        <h3 className="author-name">
-          HI THERE, I&apos;M <span className="highlight">KATHY</span>
-        </h3>
-        <p className="author-desc">
-          This blog will provide you many tested tips and advice about traveling
-          anywhere without spending a fortune
-        </p>
-      </div>
-      <div className="blog-card">
-        <div className="blog-image">
-          <Image
-            src="/images/piatra-craiului-national-park-romania.jpg"
-            alt="piatra-craiului-national-park-romania"
-            width={0}
-            height={0}
-          />
-        </div>
-        <div className="blog-content">
-          <h2 className="blog-title">Trip to Italy: Rome</h2>
-          <p className="blog-meta">
-            <span className="icon">🏷️</span> POSTED IN EUROPE
-            <span className="icon">👤</span> KATHY
-          </p>
-          <p className="blog-description">
-            This post is part of a series called Travel to Italy Palatine Hill.
-            Aliquam sit amet euismod purus, sed commodo ipsum. Morbi sodales
-            consectetur ex vitae molestie. Morbi ultrices rhoncus...
-          </p>
-          <a href="#" className="read-more">
-            CONTINUE READING
-          </a>
-        </div>
-      </div>
-      <div className="popular-posts">
-        <h2>Popular Posts</h2>
-
-        <div className="popular-post">
-          <div className="postimgcontainer">
-            <Image
-              src="/images/piatra-craiului-national-park-romania.jpg"
-              alt="piatra-craiului-national-park-romania"
-              width={0}
-              height={0}
-            />
-          </div>
-
-          <div className="popular-post-content">
-            <p className="popular-post-title">Trip to Italy: Milan</p>
-            <p className="popular-post-date">
-              <i className="fa fa-bookmark mr-2"></i>January 7, 2018
+        <div className="mt-6">
+          <div className="author-card">
+            <h4 className="author-title">AUTHOR</h4>
+            <div className="author-image">
+              <img src="https://www.dummyimage.com/300" alt="Author Image" />
+            </div>
+            <h3 className="author-name">
+              HI THERE, I&apos;M <span className="highlight">KATHY</span>
+            </h3>
+            <p className="author-desc">
+              This blog will provide you many tested tips and advice about
+              traveling anywhere without spending a fortune
             </p>
           </div>
-        </div>
+          <div class="categories">CATEGORIES</div>
+          <ul class="category-list">
+            <li class="expandable">
+              <i className="fa fa-chevron-right"></i>Europe
+            </li>
+            <li class="expandable">
+              <i className="fa fa-chevron-right"></i>Mountains
+            </li>
+            <li class="expandable">
+              <i className="fa fa-chevron-right"></i>Other
+            </li>
+            <li class="expandable">
+              <i className="fa fa-chevron-right"></i>Seas
+            </li>
+          </ul>
+          <div className="popular-posts">
+            <h2>Popular Posts</h2>
+            {Array(3)
+              .fill(0)
+              .map((item, i) => (
+                <div className="popular-post" key={i}>
+                  <div className="postimgcontainer">
+                    <Image
+                      src="/images/piatra-craiului-national-park-romania.jpg"
+                      alt="piatra-craiului-national-park-romania"
+                      width={0}
+                      height={0}
+                    />
+                  </div>
 
-        <div className="popular-post">
-          <div className="postimgcontainer">
-            <Image
-              src="/images/piatra-craiului-national-park-romania.jpg"
-              alt="piatra-craiului-national-park-romania"
-              width={0}
-              height={0}
-            />
-          </div>
-
-          <div className="popular-post-content">
-            <p className="popular-post-title">Trip to Italy: Milan</p>
-            <p className="popular-post-date">
-              <i className="fa fa-bookmark mr-2"></i>January 7, 2018
-            </p>
-          </div>
-        </div>
-
-        <div className="popular-post">
-          <div className="postimgcontainer">
-            <Image
-              src="/images/piatra-craiului-national-park-romania.jpg"
-              alt="piatra-craiului-national-park-romania"
-              width={0}
-              height={0}
-            />
-          </div>
-
-          <div className="popular-post-content">
-            <p className="popular-post-title">Trip to Italy: Milan</p>
-            <p className="popular-post-date">
-              <i className="fa fa-bookmark mr-2"></i>January 7, 2018
-            </p>
+                  <div className="popular-post-content">
+                    <p className="popular-post-title">Trip to Italy: Milan</p>
+                    <p className="popular-post-date">
+                      <i className="fa fa-bookmark mr-2"></i>January 7, 2018
+                    </p>
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
       </div>
