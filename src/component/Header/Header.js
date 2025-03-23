57aecba297056750e0cae4a72f5c1391f299e338
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-23 17:29:12 +0530

    mobile Header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,7 +1,22 @@
-import Image from 'next/image';
+import { useDeviceType } from '@/hook/useDeviceType';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const Header = () => {
+  const device = useDeviceType();
+  const router = useRouter();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleClose);
+    return () => {
+      router.events.off('routeChangeComplete', handleClose);
+    };
+  }, [router]);
   return (
     <header className="sticky-header">
       <div className="container mx-auto px-4 py-4 flexcontainer">
@@ -10,8 +25,38 @@ const Header = () => {
             <span>TIRELESS TRAVELER</span>
           </Link>
         </div>
-        <nav className="flexcontainer">
-          <ul>
+        {device === 'Desktop' || device === 'Tablet' ? (
+          <nav className="flexcontainer">
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link href="/publication">Publication</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+            </ul>
+            <div className="search-icon">
+              <i className="fa fa-search"></i>
+            </div>
+          </nav>
+        ) : (
+          <div onClick={handleShow}>
+            <i className="fa fa-bars"></i>
+          </div>
+        )}
+      </div>
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>TIRELESS TRAVELER</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ul className="slidenav-url-group">
             <li>
               <Link href="/">Home</Link>
             </li>
@@ -25,11 +70,8 @@ const Header = () => {
               <Link href="/about">About</Link>
             </li>
           </ul>
-          <div className="search-icon">
-            <i className="fa fa-search"></i>
-          </div>
-        </nav>
-      </div>
+        </Offcanvas.Body>
+      </Offcanvas>
     </header>
   );
 };
