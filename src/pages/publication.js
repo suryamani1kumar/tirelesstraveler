d0d9ea0ec0881120edf91721f108db661fe868d1
unknown <sudhanshukumar78@gmail.com>
2025-03-29 22:04:09 +0530

    content change in publiction page

--- src/pages/publication.js
+++ src/pages/publication.js
@@ -47,25 +47,24 @@ const Publication = () => {
         </div>
         <Tabs defaultActiveKey="Product Description">
           <Tab eventKey="Product Description" title="Product Description">
-            <p>
-              As a “Future Astronaut No. 326” on Virgin Galactic&apos;s space
-              venture, Arvi&apos;s thirst for exploration knows no bounds.
-              Whether capturing Africa&apos;s vibrant landscapes or trekking the
-              polar extremes, he remains a humble student of our planet&apos;s
-              contrasts: breathtaking beauty versus the challenges of human
-              civilization. His adventures underline the resilience of human
-              spirit and the endless marvels our world holds.
+            <p style={{padding:"10px"}}>
+              Arvi Singh Bahal’s journey reflects an unrelenting wanderlust and
+              entrepreneurial drive. From immigrating to the U.S. in the 1970s
+              with just $108 to building successful businesses in apparel and
+              real estate, he transformed work-related travel into a lifelong
+              quest. Visiting all 193 UN countries by 2023, he has explored over
+              two hundred territories using diverse modes, from freight trains
+              to dog sleds, accumulating over three million miles of travel.
             </p>
           </Tab>
           <Tab eventKey="Table of Contents" title="Table of Contents">
-            <p>
-              As a “Future Astronaut No. 326” on Virgin Galactic&apos;s space
-              venture, Arvi&apos;s thirst for exploration knows no bounds.
-              Whether capturing Africa&apos;s vibrant landscapes or trekking the
-              polar extremes, he remains a humble student of our planet&apos;s
-              contrasts: breathtaking beauty versus the challenges of human
-              civilization. His adventures underline the resilience of human
-              spirit and the endless marvels our world holds.
+            <p style={{padding:"10px"}}>
+              A passionate photographer with an archive of one million images,
+              Arvi documents both iconic landmarks and hidden gems. As “Future
+              Astronaut No. 326” with Virgin Galactic, his spirit of exploration
+              extends beyond Earth. Whether navigating polar extremes or
+              capturing cultural landscapes, his adventures highlight the
+              resilience of humanity and the boundless wonders of our world.
             </p>
           </Tab>
         </Tabs>
