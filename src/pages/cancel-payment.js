a9596849562d0c531999a6a8c00f7837b9d4b2f1
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-08-15 22:28:20 +0530

    change in payment

--- src/pages/cancel-payment.js
+++ src/pages/cancel-payment.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CancelPayment = () => {
-  return <div>CancelPayment</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <h1>CancelPayment</h1>
+    </div>
+  );
 };
 
 export default CancelPayment;
--- src/pages/complete-payment.js
+++ src/pages/complete-payment.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const CompletePayment = () => {
-  return <div>CompletePayment</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <h1>CompletePayment</h1>
+      <h3>
+        <Link href={"/flip-book/the-tireless-traveler-ebook.html"}>
+          view pdf
+        </Link>
+      </h3>
+    </div>
+  );
 };
 
 export default CompletePayment;
