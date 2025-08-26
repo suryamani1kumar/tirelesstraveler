263e468f6b4bc582783bdc3ceb22a8a41808527a
Suryamani kumar <8437suryamanikumar@gmail.com>
2025-08-26 13:10:26 +0100

    working on profile

--- src/component/CustomerProfile/index.js
+++ src/component/CustomerProfile/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "./UserProfileCard.scss";
+import styles from "./UserProfileCard.module.scss";
 
 export default function UserProfileCard({ name, email }) {
     return (
-        <div className={`user_card`}>
-            <div className="user_card__avatar">
+        <div className={styles.user_card}>
+            <div className={styles.user_card__avatar}>
                 <img
                     src={`/images/customer_profile.png`}
                     alt={`avatar`}
                 />
             </div>
 
-            <div className="user_card__info">
-                <span className="user_card__name">{name}</span>
-                <span className="user_card__email">{email}</span>
+            <div className={styles.user_card__info}>
+                <span className={styles.user_card__name}>{name}</span>
+                <span className={styles.user_card__email}>{email}</span>
             </div>
         </div>
     );
